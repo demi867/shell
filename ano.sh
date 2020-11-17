2df810a9b169a10096ee6c060676136daa825aa3
demi867 <73687965+demi867@users.noreply.github.com>
2020-11-17 02:09:48 -0500

    Create ano.sh

---
+++ ano.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+N=1
+echo -e "\e[1;42m * * * Starting anonsurf * * *\e[0m "
+
+(anonsurf start && anonsurf change && anonsurf myip)
+
+echo -e "\e[1;42m Let's dance now like no one see ! :) \e[0m"
+echo -e "\e[1;31m SanDemio \e[0m"
